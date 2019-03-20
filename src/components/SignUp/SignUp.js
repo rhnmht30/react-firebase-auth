718ec9467ac6ef9b95d242af5b012f24d1b8701e
Rohan <rhnmht30@gmail.com>
2019-03-20 14:32:16 +0530

    add dotenv signup page and form holders

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,7 +1,40 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-export default class SignUp extends Component {
+import * as ROUTES from "../../constants/routes";
+
+const SignUpPage = () => (
+  <div>
+    <h1>SignUp</h1>
+    <SignUpForm />
+  </div>
+);
+
+const INITIAL_STATE = {
+  username: "",
+  email: "",
+  passwordOne: "",
+  passwordTwo: "",
+  error: null
+};
+class SignUpForm extends Component {
+  state = { ...INITIAL_STATE };
+
+  onSubmit = event => {};
+
+  onChange = event => {};
+
   render() {
-    return <div />;
+    return <form onSubmit={this.onSubmit} />;
   }
 }
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+  </p>
+);
+
+export default SignUpPage;
+
+export { SignUpForm, SignUpLink };
